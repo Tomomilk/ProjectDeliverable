77486408cf37efe091e80ad63c87e1127124cdf4
Ivan Fernandez <ngtoro19@gmail.com>
2022-08-16 09:18:31 -0600

    9:18am

---
+++ CKK.Logic/Models/ShoppingCart.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CKK.Logic.Models
+{
+    class Class1
+    {
+        public Customer _customer;
+
+        private ShoppingCartItem _product1;
+        private ShoppingCartItem _product2;
+        private ShoppingCartItem _product3;
+
+        public ShoppingCart(Customer cust)
+        {
+            _customer = cust;
+        }
+
+
+
+
+
+    }
+}
