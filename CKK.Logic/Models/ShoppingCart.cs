3d60365a50868ee71a1250b77438b2c884107d6f
Ivan Fernandez <ngtoro19@gmail.com>
2022-08-16 10:41:55 -0600

    getproductbyid

--- CKK.Logic/Models/ShoppingCart.cs
+++ CKK.Logic/Models/ShoppingCart.cs
@@ -8,19 +8,71 @@ namespace CKK.Logic.Models
 {
     class Class1
     {
-        public Customer _customer;
+        private Customer _customer;
 
         private ShoppingCartItem _product1;
         private ShoppingCartItem _product2;
         private ShoppingCartItem _product3;
 
-        public ShoppingCart(Customer cust)
+        public void ShoppingCart(Customer cust)
         {
             _customer = cust;
         }
 
+        public int GetCustomerId()
+        {
+            return _id;
+        }
 
+        public ShoppingCartItem GetProductById(int id)
+        {
+            if( id == 1)
+            {
+                if( _product1 != null)
+                {
+                    return _product1;
+                }
+                else
+                {
+                    return null;
+                }
+            }
 
+            else if( id == 2)
+            {
+                if( _product2 != null)
+                {
+                    return _product2;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+
+            else if( id == 3)
+            {
+                if( _product3 != null)
+                {
+                    return _product3;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+
+            else
+            {
+                return null;
+            }
+        }
+
+        public void AddProduct(Product prod)
+        {
+            
+
+        }
 
 
     }
